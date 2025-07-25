5937c61e51e682b44259d0729f70903b580346e2
Ghizzella <ellaaajel@gmail.com>
2025-07-25 08:16:33 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Pages
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Masuk from "./pages/Masuk";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
